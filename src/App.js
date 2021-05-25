b1d1d309c9c4230a10c0a19114501e8c2bfa0655
Elena-Senicheva <79080970+Elena-Senicheva@users.noreply.github.com>
2021-05-25 23:01:28 +0300

    add Timer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
+import SelectedUserList from "./components/SelectedUserList";
+import UserList from "./components/UserList";
+import Timer from "./components/Timer";
 
-function App() {
+const usersDB = [
+  {
+    id: 1,
+    firstname: "John",
+    lastname: "Smith",
+  },
+  {
+    id: 2,
+    firstname: "Bob",
+    lastname: "Brown",
+  },
+  {
+    id: 3,
+    firstname: "Sara",
+    lastname: "Smith",
+  },
+  {
+    id: 4,
+    firstname: "Mary",
+    lastname: "Black",
+  },
+];
+
+/*
+  Перепишите Aloha, App как функциональные компоненты
+*/
+
+function App(props) {
+  const defaultUsers = usersDB.map((user) => ({
+    ...user,
+    isSelected: false,
+  }));
+  const [users, setUsers] = useState(defaultUsers);
+  const [isHidden, setIsHidden] = useState(false);
+  const hideHandler = () => setIsHidden(!isHidden);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <SelectedUserList users={users} />
       </header>
-    </div>
+      <main>
+        <button onClick={hideHandler}>Hide</button>
+        {!isHidden && <UserList users={users} setUsers={setUsers} />}
+        <Timer />
+      </main>
+    </>
   );
 }
-
 export default App;
